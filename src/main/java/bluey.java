c4dd502f8111de5615ef864028720e582c443beb
timothyloh0523 <timothyloh0523@ymail.com>
2025-01-24 14:57:13 +0800

    changed duke to bluey

---
+++ src/main/java/bluey.java
@@ -0,0 +1,7 @@
+public class bluey {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm bluey!");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye, see you again soon!");
+    }
+}
