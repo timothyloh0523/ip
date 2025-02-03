fa217bddab3741121ea4a6da9da6f6ba5ea97e0b
timothyloh0523 <timothyloh0523@ymail.com>
2025-02-03 21:44:32 +0800

    changed duke to bluey added increments for wk 3 - level 1, 2

--- src/main/java/bluey.java
+++ src/main/java/bluey.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class bluey {
     public static void main(String[] args) {
         System.out.println("Hello! I'm bluey!");
         System.out.println("What can I do for you?\n");
+        String line;
+        String[] list = new String[100];
+
+        Scanner in = new Scanner(System.in);
+        int itemCount = 0;
+        while (true) {
+            line = in.nextLine().trim();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            if (line.equalsIgnoreCase("list")) {
+                if (itemCount == 0) {
+                    System.out.println("There are no items in the list");
+                }
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i+1) + ". " + list[i]);
+                }
+            }
+            else if (itemCount<100) {
+                list[itemCount] = line.trim();
+                System.out.println("added: " + line.trim());
+                itemCount++;
+            }
+            else { // Over 100 items
+                System.out.println("List full!");
+            }
+        }
         System.out.println("Bye, see you again soon!");
     }
 }
